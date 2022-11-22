22ac57f22cf28217aa04336027ea9eb1e105dad4
InexhaustibleSnake <93211950+InexhaustibleSnake@users.noreply.github.com>
2022-11-22 13:18:41 +0300

    Added MainMenu widget

---
+++ Source/GeometryHell/Private/Character/MainPlayerController.cpp
@@ -0,0 +1,5 @@
+// Project made by Alexey Guchmazov (Inexhaustible Snake) for educational purposes
+
+
+#include "Character/MainPlayerController.h"
+
---
+++ Source/GeometryHell/Public/Character/MainPlayerController.h
@@ -0,0 +1,16 @@
+// Project made by Alexey Guchmazov (Inexhaustible Snake) for educational purposes
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+
+
+UCLASS()
+class GEOMETRYHELL_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
